0a7e11271d81cdc93d5fcff48f9f312e9b81d9c7
DebbyArends <debby_arends@live.nl>
2022-01-29 16:20:02 +0100

    Tot opdracht 2 gemaakt

--- main2.js
+++ main2.js
@@ -37,3 +37,45 @@ inventory.sort((a ,b) => {
 })
 
 console.log(inventory)
+
+//Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+
+const soldTvs = inventory.map((inventory) => {
+    return inventory.sold
+})
+
+let totalTvs = 0
+
+for (let i = 0; i < soldTvs.length; i++) {
+    totalTvs = totalTvs + soldTvs[i]
+}
+
+console.log(totalTvs)
+
+//Opdracht 2b: Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
+const totalSoldTvs = document.getElementById('sold-tvs')
+totalSoldTvs.textContent = totalTvs
+
+//Opdracht 2c: Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+
+const stockBought = inventory.map((inventory) => {
+    return inventory.originalStock
+})
+
+let totalStock = 0
+
+for (let i = 0; i < stockBought.length; i++) {
+    totalStock = totalStock + stockBought[i]
+}
+console.log(totalStock)
+
+//Opdracht 2d: Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
+const totalStockBought = document.getElementById('bought-stock')
+totalStockBought.textContent = totalStock
+
+//Opdracht 2e: Geef in het rood weer hoeveel tv's er nog verkocht moeten worden.
+const unsoldTvs = totalStock - totalTvs
+console.log(unsoldTvs)
+
+const totalUnsoldTvs = document.getElementById('unsold-tvs')
+totalUnsoldTvs.textContent = unsoldTvs
