9c41107afedc8c3f6119ef84328b7b6f7d17d3bd
DebbyArends <debby_arends@live.nl>
2022-02-02 15:12:38 +0100

    Opdracht 4 afgemaakt

--- Opdracht4.js
+++ Opdracht4.js
@@ -14,8 +14,8 @@ console.log(object(inventory[0]))
 // parameter verwacht (zoals 379) en daar de volgende string van maakt: €379,-. Test of jouw functie ook werkt
 // wanneer er een andere tv-prijs wordt meegegeven.
 
-function object2(detail) {
-    return ` €${detail.price},- `
+function object2(details) {
+    return ` €${details.price},- `
 }
 
 console.log(object2(inventory[0]))
@@ -28,36 +28,68 @@ console.log(object2(inventory[0]))
 // schermgroottes heeft ([43, 50, 55, 58]) wordt de output 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm).
 // Test of jouw functie werkt in alle mogelijke gevallen.
 function object3(details) {
-        return `${details.availableSizes} inches (${(details.availableSizes * 2.54)} cm)`
+    if (details.availableSizes.length === 1)
+        return `${details.availableSizes} inches (${ Math.round(details.availableSizes * 2.54)} cm)`
+    else {
+        let output = " "
+        for (let i = 0; i <details.availableSizes.length; i++) {
+            let inchToCm = Math.round(details.availableSizes[i] * 2.54)
+            output = `${output} ${details.availableSizes[i]} inch (${inchToCm} cm) |`
+        }
+        return output
+    }
 }
 
-console.log(object3(inventory[0]))
+console.log(object3(inventory[3]))
 
-//krijg het niet voor elkaar om meerdere schermgroottes te laten zien...?
+//Het laatste streepje moet weg
 
 //Opdracht 4d: Zorg ervoor de informatie van één van de tv's zoals het voorbeeld wordt weergegeven op de pagina.
 // Gebruik hiervoor de functies die je hebt gemaakt in opdracht 5a, 5b en 5c.
-const mergeDetails = document.getElementById('collection')
 
-const product = document.createElement('p')
-product.textContent = object(inventory[0])
-
-const price = document.createElement('p')
-price.textContent = object2(inventory[0])
-
-const screen = document.createElement('p')
-screen.textContent = object3(inventory[0])
-
-mergeDetails.appendChild(product)
-mergeDetails.appendChild(price)
-mergeDetails.appendChild(screen)
+// const mergeDetails = document.getElementById('collection')
+//
+// const product = document.createElement('div')
+// product.textContent = object(inventory[3])
+//
+// const price = document.createElement('div')
+// price.textContent = object2(inventory[3])
+//
+// const screen = document.createElement('div')
+// screen.textContent = object3(inventory[3])
+//
+// const whitespace = document.createElement('br')
+//
+// mergeDetails.appendChild(product)
+// mergeDetails.appendChild(price)
+// mergeDetails.appendChild(screen)
+// mergeDetails.appendChild(whitespace)
 
 // Opdracht 4e: Schrijf een functie die ALLE tv's weergeeft op de pagina zoals in het voorbeeld. Dit wil je natuurlijk ' +
 // 'niet acht keer opnieuw schrijven, want nu zijn het 8 tv's, maar in de toekomst misschien wel 200! Gebruik in deze functie
 // de voorgaande functies die je hebt geschreven, om onderdelen van de data te formatten. De overkoepelende "tv-generator-functie"
 // verwacht één parameter: de volledige array met tv-objecten. Vergeet 'm niet aan te roepen!
+
 function tvGenerator(arrayOfObjects) {
+    const mergeDetails = document.getElementById('collection')
+    for (let i = 0; i < arrayOfObjects.length; i++) {
+
+        const product = document.createElement('div')
+        product.textContent = object(inventory[i])
+
+        const price = document.createElement('div')
+        price.textContent = object2(inventory[i])
+
+        const screen = document.createElement('div')
+        screen.textContent = object3(inventory[i])
+
+        const whiteSpace = document.createElement('br')
+
+        mergeDetails.appendChild(product)
+        mergeDetails.appendChild(price)
+        mergeDetails.appendChild(screen)
+        mergeDetails.appendChild(whiteSpace)
+    }
 
 }
-
-tvGenerator(inventory.length)
+tvGenerator(inventory)
